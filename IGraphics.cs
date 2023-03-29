9483f379eb6ae176497186ebb5a6f3cd93040a30
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-03-28 22:22:47 -0300

    Added Clear and Rectangle operations in graphics

--- IGraphics.cs
+++ IGraphics.cs
@@ -1,3 +1,9 @@
+/* Author:  Leonardo Trevisan Silio
+ * Date:    28/03/2023
+ */
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
 namespace Pamella;
 
 /// <summary>
@@ -14,4 +20,33 @@ public interface IGraphics
     /// A height in pixels of screen.
     /// </summary>
     int Height { get; }
+
+    /// <summary>
+    /// Cursor location pixels in screen.
+    /// </summary>
+    PointF Cursor { get; }
+
+    /// <summary>
+    /// Return true if the left button in cursor is down. 
+    /// </summary>
+    bool IsDown { get; }
+
+    /// <summary>
+    /// Return true if the right button in cursor is down. 
+    /// </summary>
+    bool IsRightDown { get; }
+
+    void Clear(Color color);
+
+    void DrawRectangle(RectangleF rect, Pen pen);
+
+    void DrawRectangle(PointF loc, Size siz, Pen pen);
+
+    void DrawRectangle(float x, float y, float width, float height, Pen pen);
+
+    void FillRectangle(RectangleF rect, Brush brush);
+
+    void FillRectangle(PointF loc, Size siz, Brush brush);
+
+    void FillRectangle(float x, float y, float width, float height, Brush brush);
 }
--- Providers/WindowsForms/WindowsFormsGraphics.cs
+++ Providers/WindowsForms/WindowsFormsGraphics.cs
@@ -1,7 +1,8 @@
 /* Author:  Leonardo Trevisan Silio
  * Date:    28/03/2023
  */
-using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Pamella.Providers.WindowsForms;
 
@@ -10,12 +11,78 @@ namespace Pamella.Providers.WindowsForms;
 /// </summary>
 public class WindowsFormsGraphics : IGraphics
 {
+    private Graphics g;
+    private PictureBox pb;
+    private Bitmap bmp;
+    private PointF cursor;
+    private bool isDown;
+    private bool isRightDown;
+
     public WindowsFormsGraphics(WindowsFormsProviderArguments args)
     {
-        
+        this.g = args.Graphics;
+        this.pb = args.PictureBox;
+        this.bmp = args.Bitmap;
+        this.cursor = PointF.Empty;
+        this.isDown = false;
+
+        pb.MouseMove += (o, e) =>
+        {
+            this.cursor = e.Location;
+        };
+
+        pb.MouseDown += (o, e) =>
+        {
+            if (e.Button == MouseButtons.Left)
+                this.isDown = true;
+            
+            if (e.Button == MouseButtons.Right)
+                this.isRightDown = true;
+        };
+
+        pb.MouseUp += (o, e) =>
+        {
+            if (e.Button == MouseButtons.Left)
+                this.isDown = false;
+            
+            if (e.Button == MouseButtons.Right)
+                this.isRightDown = false;
+        };
     }
 
-    public int Width => throw new NotImplementedException();
+    public int Width
+        => this.bmp.Width;
 
-    public int Height => throw new NotImplementedException();
+    public int Height
+        => this.bmp.Height;
+
+    public PointF Cursor
+        => this.cursor;
+
+    public bool IsDown
+        => this.isDown;
+
+    public bool IsRightDown
+        => this.isRightDown;
+
+    public void Clear(Color color)
+        => g.Clear(color);
+
+    public void DrawRectangle(RectangleF rect, Pen pen)
+        => g.DrawRectangle(pen, rect.X, rect.Y, rect.Width, rect.Height);
+
+    public void DrawRectangle(PointF loc, Size siz, Pen pen)
+        => g.DrawRectangle(pen, loc.X, loc.Y, siz.Width, siz.Height);
+
+    public void DrawRectangle(float x, float y, float width, float height, Pen pen)
+        => g.DrawRectangle(pen, x, y, width, height);
+
+    public void FillRectangle(RectangleF rect, Brush brush)
+        => g.FillRectangle(brush, rect);
+
+    public void FillRectangle(PointF loc, Size siz, Brush brush)
+        => g.FillRectangle(brush, loc.X, loc.Y, siz.Width, siz.Height);
+
+    public void FillRectangle(float x, float y, float width, float height, Brush brush)
+        => g.FillRectangle(brush, x, y, width, height);
 }
