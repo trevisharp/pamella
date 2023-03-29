142872692718ffa545a42d19d400f3d5d28a3337
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-03-28 22:37:41 -0300

    Improve script to get version of package

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,35 @@
+$csproj = gc .\pamella.csproj
+$versionText = $csproj | % {
+    if ($_.Contains("PackageVersion"))
+    {
+        $_
+    }
+}
+
+$version = ""
+$flag = 0
+for ($i = 0; $i -lt $versionText.Length; $i++)
+{
+    $char = $versionText[$i]
+
+    if ($flag -eq 1)
+    {
+        if ($char -eq "<")
+        {
+            break
+        }
+
+        $version += $char
+    }
+
+    if ($char -eq ">")
+    {
+        $flag = 1
+    }
+}
+
 $key = gc .\.env
 dotnet pack -c Release
-cp .\bin\Release\pamella.1.0.0.nupkg pamella.nupkg
+cp ".\bin\Release\pamella." + $version + ".nupkg" pamella.nupkg
 dotnet nuget push pamella.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
 rm .\pamella.nupkg
