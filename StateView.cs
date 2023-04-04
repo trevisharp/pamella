f4cacc1a791a1b7b569de78f2e9aab9ca6bbb21a
Trevisan <38115923+trevisharp@users.noreply.github.com>
2023-04-04 09:29:16 -0300

    Atualizar o StateView.cs

--- StateView.cs
+++ StateView.cs
@@ -8,21 +8,27 @@ using States;
 public abstract class StateView<T> : Watcher
     where T : State
 {
+    private IGraphics crr = null;
+
     protected override void interact()
-    {
-        
-    }
+        => this.onFrame(this.State, this.crr);
 
     protected internal virtual void onStart(IGraphics g, State state) { }
     protected internal virtual void onFrame(IGraphics g, State state) { }
     protected internal virtual void onRender(IGraphics g, State state) { }
 
     protected internal override void OnFrame(IGraphics g)
-        => this.onFrame(g, state);
+    {
+        this.crr = g;
+        this.Interact();
+    }
 
     protected internal override void OnRender(IGraphics g)
-        => this.onRender(g, state);
+        => this.onRender(g, this.State);
 
     protected internal override void OnStart(IGraphics g)
-        => this.onStart(g, state);
+    {
+        this.crr = g;
+        this.onStart(g, this.State);
+    }
 }
